95b0d37d9204ea41b8e9311afc4bb6998ee17ec9
Isabeli <86092004+BelistarE@users.noreply.github.com>
2024-07-11 14:13:55 -0700

    update config

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
     new HtmlWebpackPlugin({
         template: './src/index.html',
         filename: 'index.html',
+        favicon: './images/pink-paw-print.svg',
         inject: 'body',
     }),
   ],
