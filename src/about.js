77623eafffc9f6cbfe97bf4183262d87084b8b9f
Isabeli <86092004+BelistarE@users.noreply.github.com>
2024-07-14 23:42:29 -0700

    fixed alignment on the home page

--- src/about.js
+++ src/about.js
@@ -27,7 +27,7 @@ const aboutDisplay = {
             " from fluffy omurice to decadent desserts that melt in your mouth. Savor the essence of Japan"+
             " with every sip of our carefully crafted matcha drinks, perfectly balancing tradition with innovation.<br>"+
             " In collaboration with the beloved Nekopara series, our establishment brings a touch of whimsy and warmth to every visit."+
-            " Whether you're a fan of Chocola and Vanilla or simply a lover of good food, BELI'S × ネコぱら promises a memorable dining "+
+            " Whether you're a fan of Chocola and Vanilla or simply a lover of good food, BELI'S × NEKOPARA promises a memorable dining "+
             "experience that captures the essence of both Japanese culinary tradition and playful anime culture.";
     
         const aboutDescElement = document.createElement("div");
